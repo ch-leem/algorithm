e3f354b7befff9b2a16e761281abbf3bedfc1e91
ChanHyeok Leem <chanhyeok.leem@gmail.com>
2025-05-19 01:20:10 +0900

    백준 4375 : 1 (https://www.acmicpc.net/problem/4375)

--- basic/4375/4375.cpp
+++ basic/4375/4375.cpp
@@ -1,20 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <stdio.h>
 using namespace std;
 
-int n;
+int n, ret, cnt;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-
-    while (scanf("%d", &n) != EOF){
-        int mod = 1 % n;
-        int cnt = 1;
-        while (mod != 0) {
-            mod = (mod * 10 + 1) % n;
-            cnt ++;    
+    while(scanf("%d", &n) != EOF){
+        ret = 1;
+        cnt = 1;
+        while(ret % n != 0){
+            ret = (ret * 10) + 1;
+            ret %= n;
+            cnt++;
         }
         cout << cnt << "\n";
-    } 
+    }
+
     return 0;
 }
