eaa880162435b42a9def55edd5ba74464bd1c41f
ChanHyeok Leem <chanhyeok.leem@gmail.com>
2025-06-05 18:58:16 +0900

    백준 2468 : 안전 영역 (https://www.acmicpc.net/problem/2468)

--- baekjoon/basic2/2468/2468.cpp
+++ baekjoon/basic2/2468/2468.cpp
@@ -1,3 +1,5 @@
+// 백준 2468 : 안전 영역 (https://www.acmicpc.net/problem/2468)
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -62,3 +64,54 @@ int main(){
     
     return 0;
 } 
+
+/*
+#include <iostream>
+#include <algorithm>
+using namespace std;
+const int mn = 101;
+int a[mn][mn], visited[mn][mn];
+int n, ret;
+int ny, nx, cnt;
+int dy[4] = {0,0,1,-1};
+int dx[4] = {1,-1,0,0};
+
+
+void dfs(int y, int x, int d){
+    visited[y][x] = 1;
+    for(int i=0; i<4; i++){
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny>=n || n<0 || nx>=n || nx<0) continue;
+        if(!visited[ny][nx] && a[ny][nx] > d){
+            dfs(ny, nx, d);
+        }
+    }
+    return;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> n;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int d=0; d<101; d++){
+        fill(&visited[0][0], &visited[0][0] + mn*mn, 0);
+        cnt = 0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(!visited[i][j] && a[i][j] > d){
+                    dfs(i, j, d);
+                    cnt++;
+                }
+            }
+        }
+        ret = max(ret, cnt);
+    }
+    cout << ret << "\n";
+}
+*/
