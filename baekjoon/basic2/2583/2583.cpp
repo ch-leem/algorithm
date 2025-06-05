35fe3536214f51bd4b633802462db296fbb19e46
ChanHyeok Leem <chanhyeok.leem@gmail.com>
2025-06-05 19:22:11 +0900

    백준 2583 : 영역 구하기 (https://www.acmicpc.net/problem/2583)

--- baekjoon/basic2/2583/2583.cpp
+++ baekjoon/basic2/2583/2583.cpp
@@ -57,3 +57,57 @@ int main(){
     cout << "\n";
     return 0;
 }
+/*
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#define y1 aaa
+using namespace std;
+const int mx = 101;
+int a[mx][mx], visited[mx][mx];
+int m, n, k, x1, x2, y1, y2, ny, nx;
+int dy[4] = {0,0,1,-1};
+int dx[4] = {1,-1,0,0};
+vector<int> ret;
+
+int dfs(int y, int x){
+    visited[y][x] = 1;
+    int ret = 1;
+    for(int i=0; i<4; i++){
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny>=m || ny<0 || nx>=n || nx<0) continue;
+        if(!visited[ny][nx] && a[ny][nx] != 1){
+            ret += dfs(ny, nx);
+        }
+    }
+    return ret;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> m >> n >> k;
+    for(int i=0; i<k; i++){
+        cin >> x1 >> y1 >> x2 >> y2;
+        for(int y=y1; y<y2; y++){
+            for(int x=x1; x<x2; x++){
+                a[y][x] = 1;
+            }
+        }
+    }
+    for(int i=0; i<m; i++){
+        for(int j=0; j<n; j++){
+            if(!visited[i][j] && a[i][j] != 1){
+                ret.push_back(dfs(i, j));
+            }
+        }
+    }
+    sort(ret.begin(), ret.end());
+    cout << ret.size() << '\n';
+    for(int result : ret) cout << result << ' ';
+    cout << "\n";
+
+    return 0;
+}
+*/
