29243f52870cecabacaa88decf183957a6fcca06
ChanHyeok Leem <chanhyeok.leem@gmail.com>
2025-06-30 14:57:13 +0900

    // 백준 16234 : 인구 이동 (https://www.acmicpc.net/problem/16234)

--- baekjoon/basic3/16234/16234.cpp
+++ baekjoon/basic3/16234/16234.cpp
@@ -1,107 +1,69 @@
 // 백준 16234 : 인구 이동 (https://www.acmicpc.net/problem/16234)
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 #include <cstdlib>
 using namespace std;
-
-#define move chanhyeok
-
-int n, l, r, ny, nx, person, cnt_person, cnt_kan, group;
+int n, l, r, ret, ny, nx, sum;
+bool flag;
+vector<pair<int, int>> v;
 int a[54][54], visited[54][54];
 int dy[] = {0,0,1,-1};
 int dx[] = {1,-1,0,0};
-bool move;
-vector<pair<int, int>> adj[54][54];
-vector<int> ret_person;
 
-void go(int y, int x){
+void dfs(int y, int x){
+    sum += a[y][x];
 
-    for(int d=0; d<4; d++){
-        ny = y + dy[d];
-        nx = x + dx[d];
-        if(ny >= n || ny < 0 || nx >= n || nx < 0) continue;
-        if(abs(a[y][x] - a[ny][nx]) >= l && abs(a[y][x] - a[ny][nx]) <= r){
-            adj[y][x].push_back({ny, nx});
-            move = true;
+    for(int i=0; i<4; i++){
+        ny = y + dy[i];
+        nx = x + dx[i];
+        if(ny >= n || ny < 0 || nx >= n || nx < 0 || visited[ny][nx]) continue;
+        if(abs(a[ny][nx] - a[y][x]) >= l && abs(a[ny][nx] - a[y][x]) <= r){
+            visited[ny][nx] = 1;
+            v.push_back({ny, nx});
+            dfs(ny, nx);
         }
     }
     return;
-}
-
-pair<int, int> dfs(int y, int x, int group){
-    visited[y][x] = group;
-    pair<int, int> ret = {a[y][x], 1};
-
-    for(pair<int, int> v : adj[y][x]){
-        ny = v.first;
-        nx = v.second;
-        if(visited[ny][nx] != 0) continue;
-        
-        pair<int, int> ret2 = dfs(ny, nx, group);
-
-        ret.first += ret2.first;
-        ret.second += ret2.second;
-    }
-    return ret;
 }
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 
-    cin >> n >> l >> r; 
+    cin >> n >> l >> r;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
             cin >> a[i][j];
         }
     }
-    
-    int ret = 0;
-    for(;; ret++){
-        move = false;
-        
-        // 국경선 초기화, 연합 해체
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                adj[i][j].clear();
-            }
-        }
+
+    int day = 0;
+    while (true){
+        flag = 0;
         fill(&visited[0][0], &visited[0][0] + 54*54, 0);
-    
-        // make adj : 국경선 open
         for(int i=0; i<n; i++){
             for(int j=0; j<n; j++){
-                go(i, j);
+                v.clear();
+
+                sum = 0;
+                if(visited[i][j]) continue;
+                v.push_back({i, j});
+                visited[i][j] = 1;
+                dfs(i, j);
+                if (v.size() == 1) continue;
+
+                for(pair<int, int> p : v){
+                    a[p.first][p.second] = sum / v.size();
+                    flag = 1;
+                }
             }
         }
-
-        // 연합의 인구수, 칸의 갯수
-        group = 1;
-        ret_person.clear();
-        ret_person.push_back(-1);
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(visited[i][j] != 0) continue;
-                tie(cnt_person, cnt_kan) = dfs(i, j, group);
-                person = cnt_person / cnt_kan;
-                ret_person.push_back(person);
-                group++;
-            }
-        }
-
-        // 인구 이동
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(visited[i][j] == 0) continue;
-                a[i][j] = ret_person[visited[i][j]];
-            }
-        }
-
-        if(!move) break;
-
+        if(!flag) break;    
+        day++;
     }
-    cout << ret << "\n";
+    
+    cout << day << "\n";
+    
     return 0;
 }
